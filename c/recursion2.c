828f026079b44739fe8883c7ef2f6449ef930de6
Wharrr Rrgarbl <user@localhost>
2019-03-22 14:17:41 +0300

    Code cleanup

--- c/recursion2.c
+++ c/recursion2.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int beer(int i){
-    printf("%d bottles of beer on the wall, %d bottles of beer.\n",i,i);
+    printf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down, pass it around, %d bottles of beer on the wall...\n",i,i,i-1);
     i = i - 1;
-    printf("Take one down, pass it around, %d bottles of beer on the wall...\n",i);
     if ( i == 2) {
+        printf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down, pass it around, %d bottle of beer on the wall...\n",i,i,i-1);
+        printf("%d bottle of beer on the wall, %d bottle of beer.\nTake one down, pass it around, no more bottles of beer on the wall...\n",i-1,i-1);
+        printf("No more bottles of beer on the wall, no more bottles of beer.\nWe've taken them down and passed them around; now we're drunk and passed out!");
         return 0;
     }
     else beer(i);
@@ -11,10 +13,4 @@ int beer(int i){
 int main(void){
     int i = 99;
     beer(i);
-    printf("2 bottles of beer on the wall, 2 bottles of beer.\n");
-    printf("Take one down, pass it around, 1 bottle of beer on the wall...\n");
-    printf("1 bottle of beer on the wall, 1 bottle of beer.\n");
-    printf("Take one down, pass it around, no more bottles of beer on the wall...\n");
-    printf("No more bottles of beer on the wall, no more bottles of beer.\n");
-    printf("We've taken them down and passed them around; now we're drunk and passed out!");
 }
